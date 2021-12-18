f2e782d6ee91c9dfee92b030b031c99d455ef292
Mokhtaria Benaouali <benaoualimokhtaria@hotmail.com>
2021-12-18 20:59:24 +0100

    update + function parameters

--- appJs.js
+++ appJs.js
@@ -39,3 +39,30 @@ window.speechSynthesis.onvoiceschanged = function(e) {
   loadVoice();
 };
 //create text and control parameters
+function parameters(text){
+    /*new instance représente
+    une requète de synthèse vocale. Elle contient le contenu du service permettant de définir la façon dont elle sera lu (langue, hauteur et volume).*/
+    var msg = new SpeechSynthesisUtterance();
+  
+  // Set the text.
+	msg.text = text;
+    
+    //set volume ant pitch
+    msg.volume = parseFloat(volume.value);
+    msg.rate = parseFloat(rate.value);
+    msg.pitch = parseFloat(pitch.value);
+    
+    if (voiceSelector.value) {
+		msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelector.value; })[0];
+	}
+  
+  // Queue this utterance.
+	window.speechSynthesis.speak(msg);
+
+}
+
+speakButton.addEventListener('click', function(e){
+    if (text.value.length > 0) {
+		parameters(text.value);
+	}
+});
